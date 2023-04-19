6762da9d4645a767c1b9ef306f4092565635a3cb
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-04-19 08:31:00 +0200

    global: introduce ruff as linter

--- medra_to_datacite/visitor.py
+++ medra_to_datacite/visitor.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#########################################################
 #
 # Copyright (C) 2020-2023 Graz University of Technology
 #
@@ -11,22 +11,30 @@ MedraVisitor.
 This visitor transforms a xml file represenation of the medra standard into
 json file representation of the datacite standard.
 """
-
+from typing import Union
 from xml.etree.ElementTree import Element
 
 from .utils import QName
 
 
 class MedraVisitorFactory:
+    """Factory class for MedraVisitor."""
+
     @classmethod
-    def create(cls, process_type: str, namespace: str):
+    def create(
+        cls,
+        process_type: str,
+        namespace: str,
+    ) -> Union["DOISerialArticleWork", "DOISerialIssueWork"]:
         """Create the medra visitor."""
         if process_type == "article":
             return DOISerialArticleWork(namespace)
-        elif process_type == "issue":
+
+        if process_type == "issue":
             return DOISerialIssueWork(namespace)
-        else:
-            raise ValueError("wrong schema given")
+
+        msg = "wrong schema given"
+        raise ValueError(msg)
 
 
 class MedraVisitor:
@@ -40,125 +48,114 @@ class MedraVisitor:
     }
     titles: list[dict] = []
 
-    def __init__(self, namespace):
-        """Constructor of MedraVisitor."""
+    def __init__(self, namespace: str) -> None:
+        """Construct MedraVisitor."""
         self.namespace = {"": namespace}
 
-    def process(self, node: Element):
+    def process(self, node: Element) -> None:
         """Execute the corresponding method to the tag name."""
 
-        def func_not_found(*args, **kwargs):
+        def func_not_found(*_: dict, **__: dict) -> None:
             localname = QName(node).localname
             namespace = QName(node).namespace
-            raise ValueError(f"NO visitor node: '{localname}' ns: '{namespace}'")
+            msg = f"NO visitor node: '{localname}' ns: '{namespace}'"
+            raise ValueError(msg)
 
         tag_name = QName(node).localname
         visit_func = getattr(self, f"visit_{tag_name}", func_not_found)
-        result = visit_func(node)
-        return result
+        return visit_func(node)
 
-    def append(self, key: str, value: dict):
+    def append(self, key: str, value: dict) -> None:
+        """Append to corresponding key."""
         if key not in self.node["metadata"]:
             self.node["metadata"][key] = []
 
         if value not in self.node["metadata"][key]:
             self.node["metadata"][key].append(value)
 
-    def init_node(self):
+    def init_node(self) -> None:
+        """Init node."""
         self.node = {
             "metadata": {},
             "doi": "",
             "url": "",
         }
 
-    def visit(self, node: Element):
+    def visit(self, node: Element) -> None:
         """Visit default method and entry point for the class."""
         for child in node:
             self.process(child)
 
-    def visit_Header(self, node: Element):
+    def visit_Header(self, node: Element) -> None:
         """Visit method for Header tag."""
-        pass
 
-    def visit_NotificationType(self, node: Element):
+    def visit_NotificationType(self, node: Element) -> None:
         """Visit method for NotificationType tag."""
-        pass
 
-    def visit_DOI(self, node: Element):
+    def visit_DOI(self, node: Element) -> None:
         """Visit method for DOI tag."""
         self.node["doi"] = node.text
 
-    def visit_DOIWebsiteLink(self, node: Element):
+    def visit_DOIWebsiteLink(self, node: Element) -> None:
         """Visit method for DOIWebsiteLink tag."""
         self.node["url"] = node.text
 
-    def visit_DOIStructuralType(self, node: Element):
+    def visit_DOIStructuralType(self, node: Element) -> None:
         """Visit method for DOIStructuralType tag."""
-        pass
 
-    def visit_RegistrantName(self, node: Element):
+    def visit_RegistrantName(self, node: Element) -> None:
         """Visit method for RegistrantName tag."""
         self.node["metadata"]["publisher"] = node.text
 
-    def visit_RegistrationAuthority(self, node: Element):
+    def visit_RegistrationAuthority(self, node: Element) -> None:
         """Visit method for RegistrationAuthority tag."""
-        pass
 
-    def visit_WorkIdentifier(self, node: Element):
+    def visit_WorkIdentifier(self, node: Element) -> None:
         """Visit method for WorkIdentifier tag."""
-        pass
 
-    def visit_SerialPublication(self, node: Element):
+    def visit_SerialPublication(self, node: Element) -> None:
         """Visit method for SerialPublication tag."""
         self.visit(node)
 
-    def visit_SerialWork(self, node: Element):
+    def visit_SerialWork(self, node: Element) -> None:
         """Visit method for SerialWork tag."""
-        pass
 
-    def visit_SerialVersion(self, node: Element):
+    def visit_SerialVersion(self, node: Element) -> None:
         """Visit method for SerialVersion tag."""
-        pass
 
-    def visit_Title(self, node: Element):
+    def visit_Title(self, node: Element) -> None:
         """Visit method for Title tag."""
         self.titles.append(
             {
                 "title": node.find("TitleText", self.namespace).text,
                 "lang": node.get("language"),
-            }
+            },
         )
 
-    def visit_Publisher(self, node: Element):
+    def visit_Publisher(self, node: Element) -> None:
         """Visit method for Publisher tag."""
         publisher = node.find("PublisherName", self.namespace).text
         self.node["metadata"]["publisher"] += f" & {publisher}"
 
-    def visit_CountryOfPublication(self, node: Element):
+    def visit_CountryOfPublication(self, node: Element) -> None:
         """Visit method for CountryOfPublication tag."""
-        pass
 
-    def visit_JournalIssue(self, node: Element):
+    def visit_JournalIssue(self, node: Element) -> None:
         """Visit method for JournalIssue tag."""
-        pass
 
-    def visit_JournalVolumeNumber(self, node: Element):
+    def visit_JournalVolumeNumber(self, node: Element) -> None:
         """Visit method for JournalVolumeNumber tag."""
-        pass
 
-    def visit_JournalIssueNumber(self, node: Element):
+    def visit_JournalIssueNumber(self, node: Element) -> None:
         """Visit method for JournalIssueNumber tag."""
-        pass
 
-    def visit_JournalIssueDesignation(self, node: Element):
+    def visit_JournalIssueDesignation(self, node: Element) -> None:
         """Visit method for JournalIsseDesignation tag."""
-        pass
 
-    def visit_JournalIssueDate(self, node: Element):
+    def visit_JournalIssueDate(self, node: Element) -> None:
         """Visit method for JournalIssueDate tag."""
-        pass
 
-    def visit_ContentItem(self, node: Element):
+    def visit_ContentItem(self, node: Element) -> None:
         """Visit method for ContentItem tag."""
         self.titles = []
         self.abstracts = []
@@ -171,15 +168,13 @@ class MedraVisitor:
         for abstract in self.abstracts:
             self.append("descriptions", abstract)
 
-    def visit_SequenceNumber(self, node: Element):
+    def visit_SequenceNumber(self, node: Element) -> None:
         """Visit method for SequenceNumber tag."""
-        pass
 
-    def visit_TextItem(self, node: Element):
+    def visit_TextItem(self, node: Element) -> None:
         """Visit method for TextItem tag."""
-        pass
 
-    def visit_Contributor(self, node: Element):
+    def visit_Contributor(self, node: Element) -> None:
         """Visit method for Contributor tag."""
         self.contributor = {}
         self.affiliations = []
@@ -191,11 +186,10 @@ class MedraVisitor:
         # NOTE: there seams not to be a creators in medra
         self.node["metadata"]["creators"].append(self.contributor)
 
-    def visit_ContributorRole(self, node: Element):
+    def visit_ContributorRole(self, node: Element) -> None:
         """Visit method for ContributorRole tag."""
-        pass
 
-    def visit_NameIdentifier(self, node: Element):
+    def visit_NameIdentifier(self, node: Element) -> None:
         """Visit method for NameIdentifier tag."""
         name_id_type = node.find("NameIDType", self.namespace).text
 
@@ -206,69 +200,62 @@ class MedraVisitor:
                 "schemeURI": "https://orcid.org/",
             }
         else:
-            raise ValueError("type not yet implemented")
+            msg = "type not yet implemented"
+            raise ValueError(msg)
 
-    def visit_PersonName(self, node: Element):
+    def visit_PersonName(self, node: Element) -> None:
         """Visit method for PersonName tag."""
-        pass
 
-    def visit_PersonNameInverted(self, node: Element):
+    def visit_PersonNameInverted(self, node: Element) -> None:
         """Visit method for PersonNameInverted tag."""
         self.contributor["name"] = node.text
         self.contributor["nameType"] = "Personal"
 
-    def visit_KeyNames(self, node: Element):
+    def visit_KeyNames(self, node: Element) -> None:
         """Visit method for KeyNames tag."""
-        pass
 
-    def visit_NamesBeforeKey(self, node: Element):
+    def visit_NamesBeforeKey(self, node: Element) -> None:
         """Visit method for NamesBeforeKey tag."""
-        pass
 
-    def visit_ProfessionalAffiliation(self, node: Element):
+    def visit_ProfessionalAffiliation(self, node: Element) -> None:
         """Visit method for ProfessionalAffiliation tag."""
         self.affiliations.append(
-            {"name": node.find("Affiliation", self.namespace).text}
+            {"name": node.find("Affiliation", self.namespace).text},
         )
 
-    def visit_Language(self, node: Element):
+    def visit_Language(self, node: Element) -> None:
         """Visit method for Language tag."""
-        pass
 
-    def visit_OtherText(self, node: Element):
+    def visit_OtherText(self, node: Element) -> None:
         """Visit method for OtherText tag."""
         self.abstracts.append(
             {
                 "description": node.find("Text", self.namespace).text.strip(),
                 "descriptionType": "Abstract",
-            }
+            },
         )
 
-    def visit_PublicationDate(self, node: Element):
+    def visit_PublicationDate(self, node: Element) -> None:
         """Visit method for PublicationDate tag."""
         self.node["metadata"]["publicationYear"] = node.text[:4]
 
-    def visit_RelatedWork(self, node: Element):
+    def visit_RelatedWork(self, node: Element) -> None:
         """Visit method for RelatedWork tag."""
-        pass
 
-    def visit_RelationCode(self, node: Element):
+    def visit_RelationCode(self, node: Element) -> None:
         """Visit method for RelationCode tag."""
-        pass
 
-    def visit_RelatedProduct(self, node: Element):
+    def visit_RelatedProduct(self, node: Element) -> None:
         """Visit method for RelatedProduct tag."""
-        pass
 
-    def visit_BiographicalNote(self, node: Element):
+    def visit_BiographicalNote(self, node: Element) -> None:
         """Visit method for BiographicalNote tag."""
-        pass
 
 
 class DOISerialArticleWork(MedraVisitor):
     """DOISerialArticleWork class."""
 
-    def visit_DOISerialArticleWork(self, node: Element):
+    def visit_DOISerialArticleWork(self, node: Element) -> None:
         """Visit method for DOISerialArticleWork tag."""
         self.init_node()
 
@@ -280,12 +267,12 @@ class DOISerialArticleWork(MedraVisitor):
         }
         self.node["metadata"]["schemaVersion"] = "http://datacite.org/schema/kernel-4"
         self.node["metadata"]["identifiers"] = [
-            {"identifierType": "DOI", "identifier": self.node["doi"]}
+            {"identifierType": "DOI", "identifier": self.node["doi"]},
         ]
 
         self.nodes.append(self.node)
 
-    def visit_SerialWork(self, node: Element):
+    def visit_SerialWork(self, node: Element) -> None:
         """Visit method for SerialWork tag."""
         self.titles = []
         self.visit(node)
@@ -299,11 +286,15 @@ class DOISerialArticleWork(MedraVisitor):
 
 
 class DOISerialIssueWork(MedraVisitor):
-    def init_node(self):
+    """DOISerialIssueWork class."""
+
+    def init_node(self) -> None:
+        """Init node."""
         super().init_node()
         self.node["metadata"] = {"creators": [{"name": ":none"}]}
 
-    def visit_DOISerialIssueWork(self, node: Element):
+    def visit_DOISerialIssueWork(self, node: Element) -> None:
+        """Visit method for DOISerialIssueWork tag."""
         self.init_node()
         self.abstracts = []
 
@@ -318,12 +309,12 @@ class DOISerialIssueWork(MedraVisitor):
         }
         self.node["metadata"]["schemaVersion"] = "http://datacite.org/schema/kernel-4"
         self.node["metadata"]["identifiers"] = [
-            {"identifierType": "DOI", "identifier": self.node["doi"]}
+            {"identifierType": "DOI", "identifier": self.node["doi"]},
         ]
 
         self.nodes.append(self.node)
 
-    def visit_WorkIdentifier(self, node: Element):
+    def visit_WorkIdentifier(self, node: Element) -> None:
         """Visit method for WorkIdentifier tag."""
         if node.find("WorkIDType", self.namespace).text != "06":
             return
@@ -337,7 +328,7 @@ class DOISerialIssueWork(MedraVisitor):
             },
         )
 
-    def visit_SerialWork(self, node: Element):
+    def visit_SerialWork(self, node: Element) -> None:
         """Visit method for SerialWork tag."""
         self.titles = []
 
@@ -345,17 +336,17 @@ class DOISerialIssueWork(MedraVisitor):
 
         self.node["metadata"]["titles"] = self.titles
 
-    def visit_JournalIssue(self, node: Element):
+    def visit_JournalIssue(self, node: Element) -> None:
         """Visit method for JournalIssue tag."""
         self.visit(node)
 
-    def visit_JournalIssueDesignation(self, node: Element):
+    def visit_JournalIssueDesignation(self, node: Element) -> None:
         """Visit method for JournalIsseDesignation tag."""
         self.append(
             "descriptions",
             {"description": node.text, "descriptionType": "SeriesInformation"},
         )
 
-    def visit_RelatedWork(self, node: Element):
+    def visit_RelatedWork(self, node: Element) -> None:
         """Visit method for RelatedWork tag."""
         self.visit(node)
